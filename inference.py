66a052add4b917e20279a93eda640feba79f87d7
Jonah Kornberg <kornberg.jonah@gmail.com>
2023-02-20 20:27:30 -0500

    initial commit

--- inference.py
+++ inference.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-
+import importlib
 import gc
 import pathlib
 import sys
@@ -7,6 +7,7 @@ import sys
 import gradio as gr
 import PIL.Image
 import numpy as np
+from omegaconf import OmegaConf
 
 import torch
 from diffusers import StableDiffusionPipeline
@@ -32,23 +33,65 @@ class InferencePipeline:
         curr_dir = pathlib.Path(__file__).parent
         return curr_dir / name
 
+
+    def get_obj_from_str(self, string, reload=False):
+        module, cls = string.rsplit(".", 1)
+        if reload:
+            module_imp = importlib.import_module(module)
+            importlib.reload(module_imp)
+        return getattr(importlib.import_module(module, package=None), cls)
+    
+    def instantiate_from_config(self, config):
+        if not "target" in config:
+            if config == '__is_first_stage__':
+                return None
+            elif config == "__is_unconditional__":
+                return None
+            raise KeyError("Expected key `target` to instantiate.")
+        return self.get_obj_from_str(config["target"])(**config.get("params", dict()))
+
+    def load_model_from_config(self, config, ckpt, verbose=False):
+        print(f"Loading model from {ckpt}")
+        pl_sd = torch.load(ckpt, map_location="cpu")
+        if "global_step" in pl_sd:
+            print(f"Global Step: {pl_sd['global_step']}")
+        sd = pl_sd["state_dict"]
+        model = self.instantiate_from_config(config.model)
+
+        token_weights = sd["cond_stage_model.transformer.text_model.embeddings.token_embedding.weight"]
+        del sd["cond_stage_model.transformer.text_model.embeddings.token_embedding.weight"]
+        m, u = model.load_state_dict(sd, strict=False)
+        model.cond_stage_model.transformer.text_model.embeddings.token_embedding.weight.data[:token_weights.shape[0]] = token_weights
+        if len(m) > 0 and verbose:
+            print("missing keys:")
+            print(m)
+        if len(u) > 0 and verbose:
+            print("unexpected keys:")
+            print(u)
+
+        model.cuda()
+        model.eval()
+        return model
+
     def load_pipe(self, model_id: str, filename: str) -> None:
+        print('start')
         weight_path = self.get_weight_path(filename)
         if weight_path == self.weight_path:
             return
         self.weight_path = weight_path
+        print("loading")
         weight = torch.load(self.weight_path, map_location=self.device)
-
+        print("loaded weight")
+        #config = OmegaConf.load(f"config/finetune.yaml")
+        #model = self.load_model_from_config(config, self.weight_path)
         if self.device.type == 'cpu':
             pipe = StableDiffusionPipeline.from_pretrained(model_id)
         else:
             pipe = StableDiffusionPipeline.from_pretrained(
                 model_id, torch_dtype=torch.float16)
             pipe = pipe.to(self.device)
-
         from src import diffuser_training
         diffuser_training.load_model(pipe.text_encoder, pipe.tokenizer, pipe.unet, weight_path, compress=False)
-
         self.pipe = pipe
 
     def run(
@@ -65,9 +108,9 @@ class InferencePipeline:
     ) -> PIL.Image.Image:
         if not torch.cuda.is_available():
             raise gr.Error('CUDA is not available.')
-
+        print("running")
         self.load_pipe(base_model, weight_name)
-
+        print("loaded pipe")
         generator = torch.Generator(device=self.device).manual_seed(seed)
         out = self.pipe([prompt]*batch_size,
                         num_inference_steps=n_steps,
